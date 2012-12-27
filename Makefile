3f23e654014fe246f2bcc46a551a9c50f64a0cf3
Preetam Jinka <pj@isomero.us>
2012-12-26 19:36:02 -0500

    Switching to rows.

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 test: src/main.c
 	$(CC) $^ -std=c99 -o bin/advs
+
+run:
 	./bin/advs
+
+check:
+	cat db.advs | hexdump -v -e '"\\\x" 1/1 "%02x" " "'
 
 .PHONY: test
--- src/main.c
+++ src/main.c
@@ -1,26 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+
+typedef struct {
+	uint32_t timestamp;
+	float in;
+	float out;
+} row;
 
 void writeHeader(FILE *p);
 void writeFloatToFile(FILE *fp, int nth, float val);
 float readFloatFromFile(FILE *fp, int nth);
+void writeRowToFile(FILE *fp, int location, row r);
+row readRowFromFile(FILE *fp, int location);
+void printRow(row r);
 
 int main(void) {
 	FILE *fp = fopen("db.advs", "w+");
 	writeHeader(fp);
 	writeFloatToFile(fp, 0, 1);
 	int i;
+
+/*
 	for(i = 0; i < 1000000; i++) {
 		writeFloatToFile(fp, i, 3.4028234e38);
 	}
+*/
+
+	for(i = 0; i < 26554; i++) {
+		row r = {1356387320+i, ((float) rand() / (RAND_MAX))*20, ((float) rand() / (RAND_MAX))*20};
+		writeRowToFile(fp, i, r);
+	}
+
 	fclose(fp);
 	
 	fp = fopen("db.advs", "rb");
-
+	for(i = 0; i < 10; i++) {
+		row r = readRowFromFile(fp, i);
+		printRow(r);
+	}
+/*
 	for(i = 0; i < 100; i++) {
 		printf("[%d] => %f\n", i,readFloatFromFile(fp, i));
 	}
+*/
 	fclose(fp);
 
 	return EXIT_SUCCESS;
@@ -29,6 +53,40 @@ int main(void) {
 void writeHeader(FILE *fp) {
 	char *header = "ADVS";
 	fprintf(fp, "%s", header);
+}
+
+void writeRowToFile(FILE *fp, int location, row r) {
+	char inBuffer[4] = {0};
+	char outBuffer[4] = {0};
+	fseek(fp, location*12, 4);
+	memcpy(inBuffer, &(r.in), 4);
+	memcpy(outBuffer, &(r.out), 4);
+	fwrite(&(r.timestamp), 4, 1, fp);
+	fwrite(inBuffer, 4, 1, fp);
+	fwrite(outBuffer, 4, 1, fp);
+}
+
+row readRowFromFile(FILE *fp, int location) {
+	char inBuffer[4] = {0};
+	char outBuffer[4] = {0};
+	uint32_t timestamp = 0;
+
+	fseek(fp, 12*location+8, SEEK_SET);
+	fread(&timestamp, 4, 1, fp);
+	fread(inBuffer, 4, 1, fp);
+	fread(outBuffer, 4, 1, fp);
+
+	float in = 0;
+	float out = 0;
+	memcpy(&in, inBuffer, 4);
+	memcpy(&out, outBuffer, 4);
+
+	row r = {timestamp, in, out};
+	return r;
+};
+
+void printRow(row r) {
+	printf("[ %d, %f, %f ]\n", r.timestamp, r.in, r.out);
 }
 
 void writeFloatToFile(FILE *fp, int nth, float val) {
